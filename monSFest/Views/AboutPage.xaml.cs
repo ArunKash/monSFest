d8839fa8e261bb0e1c7b5250ee4b30fa757d9944
ArunKash <arun.prakash@cognizant.com>
2017-09-23 19:48:56 +0530

    Initial check-in of module monSFest

---
+++ monSFest/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace monSFest
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
