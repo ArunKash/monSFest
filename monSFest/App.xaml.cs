c76b1631d5255e53166238b12bf89c816a6b5bdb
arunKash <arun.prakash@cognizant.com>
2017-12-24 19:58:32 +0530

    Fetching Access Token by Username Password flow

--- monSFest/App.xaml.cs
+++ monSFest/App.xaml.cs
@@ -2,6 +2,8 @@
 using Rg.Plugins.Popup.Extensions;
 
 using Xamarin.Forms;
+using monSFest.WebServices;
+using System.Threading.Tasks;
 
 namespace monSFest
 {
@@ -20,20 +22,34 @@ namespace monSFest
             //  DependencyService.Register<CloudDataStore>();
             try
             {
-                displayPopUp();
+                displayPopUpWithMessage("Live a Little!");
             }
-            catch{
-                System.Diagnostics.Debug.Print("Could Not Put it !!");
+            catch(Exception e){
+                System.Diagnostics.Debug.Print("Could Not Put it !!"+e.Message+e.Source+e);
             }
             if (Device.RuntimePlatform == Device.iOS)
-                MainPage = new LoginControls.LoginPage("https://login.salesforce.com");
+                MainPage = new MainPage();
+                //MainPage = new LoginControls.LoginPage("https://login.salesforce.com");
             else
-                MainPage = new NavigationPage(new LoginControls.LoginPage("https://login.salesforce.com"));
+                MainPage = new NavigationPage(new MainPage());
+            Task<bool> Success = WebServices.Webservice.Instance.Connect(LoginControls.ConnectionInfos.Instance);
+
+			//WebServices.Webservice.Instance.Connect(LoginControls.ConnectionInfos.Instance);
+			MainPage = new NavigationPage(new LoginControls.LoginPage("https://login.salesforce.com"));
             MainPage.Title = "Admin Login";
         }
         void displayPopUp(){
-			App.Current.MainPage.Navigation.PushPopupAsync(new InitialPleaseWait());
+            InitialPleaseWait page = new InitialPleaseWait();
+            App.Current.MainPage.Navigation.PushPopupAsync(page);
 
 		}
+        void displayPopUpWithMessage(string message)
+		{
+            InitialPleaseWait page = new InitialPleaseWait(message);
+
+            App.Current.MainPage.Navigation.PushPopupAsync(page);
+
+		}
+
     }
 }
--- monSFest/Views/InitialPleaseWait.xaml.cs
+++ monSFest/Views/InitialPleaseWait.xaml.cs
@@ -1,10 +1,19 @@
 ﻿using Rg.Plugins.Popup.Pages; using System; using System.Collections.Generic; using System.Linq; using System.Text; using System.Threading.Tasks;  using Xamarin.Forms;  namespace monSFest {
 	public partial class InitialPleaseWait : PopupPage
 	{
-		public InitialPleaseWait()
+        string Label;         public  InitialPleaseWait()
 		{
             System.Diagnostics.Debug.Print("The Log is Presented");             InitializeComponent();
 
-			//this.lblMsg.Text = strMessge;
+			this.lblMsg.Text = "Getting you Started . . . ";
+		}         public InitialPleaseWait(string message) {             this.Label = message ;             InitializeComponent();         }
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
 		}
 	} }  
